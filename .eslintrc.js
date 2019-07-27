30c1a96b6fd637e79eb2a50e64d050bfaae283a7
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-07-27 15:17:44 +0700

    extend eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'space-in-parens': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
