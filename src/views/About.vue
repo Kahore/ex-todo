f055f805010aff703eb5d1d046571e8da0fbc05f
Kahore <konstantin.golubkin@hotmail.com>
2019-07-28 14:16:53 +0800

    add info to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>About page</h1>
+		<section class="decs">
+			<p>This is a simple todo app allows:</p>
+			<ul>
+				<li>create todo task with planned date of expiration</li>
+				<li>edit todo task</li>
+				<li>mark todo task</li>
+				<li>delete todo task</li>
+			</ul>
+			<p>You also can filter you todo list with 2 extended conditions:</p>
+			<ul>
+				<li>only in progress</li>
+				<li>only completed</li>
+			</ul>
+		</section>
+	</div>
 </template>
+<style scoped>
+.decs {
+	text-align: left;
+}
+</style>
+
