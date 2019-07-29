34c9d2620624dad3020ae7b209b193347c743901
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-07-29 21:46:18 +0700

    refactoring + new tests

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,152 +1,180 @@
 <template>
-	<div class="todos-list">
-		<div
-			v-for="(todo, id ) in todos"
-			:key="id"
-			class="todos-list_block"
-			:class="{ 'todos-list_block__done': todo.completed }"
-		>
-			<div :id="todo.id">
-				<input type="checkbox" :value="todo.id" @change="markComplete( todo )" v-model="completedTODO" />
-				<span class="todos-list_block-text" @dblclick="editTodo(todo)">{{ todo.title }}</span>
-				<span class="todos-list_block-text todos-list_block-text--right">{{ todo.dateExp }}</span>
-				<input
-					class="edit"
-					type="text"
-					v-model="todo.title"
-					@blur="doneEdit(todo)"
-					@keyup.enter="doneEdit(todo)"
-					@keyup.esc="cancelEdit(todo)"
-				/>
-				<button class="btn-del" @click="delTodo(todo.id)"></button>
-			</div>
-		</div>
-	</div>
+  <div class="todos-list">
+    <div
+      v-for="(todo, id ) in todos"
+      :key="id"
+      class="todos-list__block"
+      :class="{ 'todos-list__block__done': todo.completed, 'todos-list__block--onEdit': todo == editedTodo }"
+      :id="todo.id"
+    >
+        <div class="todos-list__block_view" @dblclick="editTodo(todo)">
+          <input type="checkbox" :value="todo.id" @change="markComplete( todo )" v-model="completedTODO" />
+          <span class="todos-list__block-text" >{{ todo.title }}</span>
+          <span class="todos-list__block-text todos-list__block-text_right">{{ todo.dateExp }}</span>
+					<button class="btn-del" @click="delTodo(todo.id)"></button>
+        </div>
+        <div class="todos-list__block_edit">
+          <input
+            class="todos-list__block-text"
+            id="editTitle"
+            type="text"
+            v-model.trim="todo.title"
+						required
+            @blur="editDone(todo)"
+            @keyup.enter="editDone(todo)"
+            @keyup.esc="editCancel(todo)"
+          />
+					<input
+            class="todos-list__block-text todos-list__block-text_right"
+            id="editDateExp"
+            type="date"
+						required
+            v-model="todo.dateExp"
+            @blur="editDone(todo)"
+            @keyup.enter="editDone(todo)"
+            @keyup.esc="editCancel(todo)"
+          />
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "TodoList",
-	data() {
-		return {
-			onEdit: true,
-			editedTodo: null,
-			editedTitle: "",
-			editedDateExp: ""
-		};
-	},
-	methods: {
-		markComplete(todo) {
-			todo.completed = !todo.completed;
-			this.$store.dispatch("MUTATE_TODO_MARK", todo);
-		},
-		editTodo(todo) {
-			(this.tmpTitle = todo.title),
-				(this.tmpDateExp = todo.dateExp),
-				(this.editedTodo = todo);
-			console.log("TCL: editTodo -> editedTodo", this.editedTodo);
-		},
-		doneEdit: function(todo) {
-			// if (!this.editedTodo) {
-			//   return
-			// }
-			// this.editedTodo = null
-			// todo.title = todo.title.trim()
-			// if (!todo.title) {
-			//   this.removeTodo(todo)
-			// }
-		},
+  name: 'TodoList',
+  data(){
+    return {
+      editedTodo: null,
+      tmpTitle: '',
+      tmpDateExp: ''
+    }
+  },
+  methods: {
+    markComplete ( todo ) {
+      todo.completed = !todo.completed;
+      this.$store.dispatch( 'MUTATE_TODO_MARK', todo );
+    },
+    editTodo ( todo ) {
+      this.tmpTitle = todo.title;
+      this.tmpDateExp = todo.dateExp;
+      this.editedTodo = todo;
+    },
+    editDone ( todo ) {
+      // if (!this.editedTodo) {
+      //   return
+      // }
+      this.editedTodo = null
+      
+      // todo.title = todo.title.trim()
+      // if (!todo.title) {
+      //   this.removeTodo(todo)
+      // }
+    },
 
-		cancelEdit: function(todo) {
-			// this.editedTodo = null
-			// todo.title = this.beforeEditCache
-		},
+    editCancel ( todo ) {
+      this.editedTodo = null;
+      todo.title = this.tmpTitle;
+      todo.dateExp = this.tmpDateExp;
+    },
 
-		delTodo(todoId) {
-			this.$store.dispatch("MUTATE_TODO_DELETE", todoId);
-		}
-	},
-	computed: {
-		todos() {
-			return this.$store.getters.GET_TODOS;
-		},
-		completedTODO: {
-			get: function() {
-				return this.$store.getters.GET_TODO_COMPLETED;
-			},
-			set: function(value) {
-				this.$emit("input", value);
-			}
-		}
-	},
-	created() {
-		this.$store.dispatch("LOAD_TODO");
-	}
+    delTodo ( todoId ) {
+      this.$store.dispatch( 'MUTATE_TODO_DELETE', todoId );
+    }
+  },
+  computed: {
+    todos () {
+      return this.$store.getters.GET_TODOS;
+    },
+    completedTODO: {
+      get: function () {
+        return this.$store.getters.GET_TODO_COMPLETED;
+      },
+      set: function ( value ) {
+        this.$emit( 'input', value );
+      }
+    }
+  },
+  created () {
+    this.$store.dispatch( 'LOAD_TODO' );
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.todos-list_block {
-	position: relative;
-	font-size: 24px;
-	border-bottom: 1px solid #ededed;
-	text-align: left;
-	&:hover .btn-del {
-		display: inline-block;
+.todos-list__block {
+  position: relative;
+  font-size: 24px;
+  border-bottom: 1px solid #ededed;
+  text-align: left;
+  &:hover {
+		background: lighten($color: darkgray, $amount: 30%);
+		& .btn-del {
+    	display: inline-block;
+  	}
 	}
-	&-text {
-		display: inline-flex;
-		word-break: break-word;
-		transition: color 0.4s;
-		max-width: 260px;
-		margin-left: 40px;
-		&--right {
-			float: right;
-			padding-right: 60px;
-		}
-	}
-	&__done {
-		text-decoration: line-through;
-	}
+  &-text {
+    display: inline-flex;
+    word-break: break-word;
+    transition: color 0.4s;
+    max-width: 260px;
+    margin-left: 40px;
+    &_right {
+      float: right;
+      padding-right: 60px;
+    }
+  }
+  &_edit {
+   display: none;
 }
+  &__done {
+    text-decoration: line-through;
+  }
+ &--onEdit .todos-list__block_view{
+    display: none;
+ }
+  &--onEdit .todos-list__block_edit{
+   display: block;
+ }
+}
+
 .btn-del {
-	display: none;
-	position: absolute;
-	right: 10px;
-	width: 40px;
-	height: 40px;
-	margin: auto 0;
-	font-size: 30px;
-	color: #cc9a9a;
-	-webkit-transition: color 0.2s ease-out;
-	transition: color 0.2s ease-out;
-	&:hover {
-		color: #af5b5e;
-		cursor: pointer;
-	}
-	&:after {
-		content: "×";
-	}
+  display: none;
+  position: absolute;
+  right: 10px;
+  width: 40px;
+  height: 40px;
+  margin: auto 0;
+  font-size: 30px;
+  color: #cc9a9a;
+  -webkit-transition: color 0.2s ease-out;
+  transition: color 0.2s ease-out;
+  &:hover {
+    color: #af5b5e;
+    cursor: pointer;
+  }
+  &:after {
+    content: "×";
+  }
 }
 @media screen and (max-width: 375px) {
-	.todos-list_block {
-		&-text {
-			max-width: 130px;
-			&--right {
-				padding-right: 25px;
-			}
-		}
-	}
+  .todos-list__block {
+    &-text {
+      max-width: 130px;
+      &_right {
+        padding-right: 25px;
+      }
+    }
+  }
 }
 @media screen and (max-width: 425px) {
-	.todos-list_block {
-		font-size: 12px;
-	}
-	.btn-del {
-		width: 10px;
-		height: 0px;
-		font-size: 15px;
-	}
+  .todos-list__block {
+    font-size: 12px;
+  }
+  .btn-del {
+    width: 10px;
+    height: 0px;
+    font-size: 15px;
+  }
 }
 </style>
--- tests/unit/TodoList.spec.js
+++ tests/unit/TodoList.spec.js
@@ -4,8 +4,14 @@ import TodoList from '@/components/TodoList.vue';
 
 const localVue = createLocalVue();
 localVue.use( Vuex );
-
+const fakeData = {
+  id: '098cf784-e75a-4d1d-b64e-d6b4fb2f3a88',
+  title: 'my first todo',
+  dateExp: '2019-07-27',
+  completed: true
+};
 describe( 'TodoList.vue', () => {
+  let editTodo = jest.fn();
   let store;
   let getters;
   let actions;
@@ -40,7 +46,7 @@ describe( 'TodoList.vue', () => {
       localVue,
       attachToDocument: true
     } );
-    expect( wrapper.findAll( '.todos-list_block' ).length ).toBe( 2 );
+    expect( wrapper.findAll( '.todos-list__block' ).length ).toBe( 2 );
     wrapper.destroy();
   } );
   it( 'should rise markComplete meth when click on checkbox', () => {
@@ -61,6 +67,103 @@ describe( 'TodoList.vue', () => {
     } );
     wrapper.find( '.btn-del' ).trigger( 'click' );
     expect( wrapper.emitted( 'delTodo' ) );
+    wrapper.destroy();
+  } );
+  it( 'should rise editTodo on todos-list__block_view', () => {
+    const wrapper = shallowMount( TodoList, {
+      store,
+      localVue,
+      attachToDocument: true
+    } );
+    wrapper.setMethods( { editTodo: editTodo } );
+    // wrapper.setData( { editedTodo: fakeData, tmpTitle: '', tmpDateExp: '' } );
+    wrapper.find( '.todos-list__block_view' ).trigger( 'dblclick' );
+    expect( editTodo ).toHaveBeenCalledWith( fakeData );
+    // expect( editedTodo ).toEqual( fakeData );
+
+    wrapper.destroy();
+  } );
+  it( 'should rise editDone on blur event in edit mode ', () => {
+    const wrapper = shallowMount( TodoList, {
+      store,
+      localVue,
+      attachToDocument: true
+    } );
+    let editDone = jest.fn();
+    let editTodo = jest.fn();
+    wrapper.setMethods( { editTodo: editTodo, editDone: editDone } );
+    wrapper.find( '.todos-list__block_view' ).trigger( 'dblclick' );
+    const childInput = wrapper.find( '#editTitle' );
+
+    childInput.trigger( 'click' );
+    expect( editTodo ).toHaveBeenCalledWith( fakeData );
+    childInput.trigger( 'blur' );
+    expect( wrapper.vm.editedTodo ).toBe( null );
+    expect( editDone ).toBeCalled();
+    wrapper.destroy();
+  } );
+  it( 'should rise editDone on enter key in edit mode ', () => {
+    let editDone = jest.fn();
+
+    const wrapper = shallowMount( TodoList, {
+      store,
+      localVue,
+      attachToDocument: true
+    } );
+    wrapper.setMethods( { editDone: editDone } );
+    wrapper.find( '.todos-list__block_view' ).trigger( 'dblclick' );
+    const childInput = wrapper.find( '#editTitle' );
+
+    childInput.trigger( 'click' );
+    childInput.trigger( 'keyup.enter' );
+    expect( editDone ).toBeCalled();
+    wrapper.destroy();
+  } );
+
+  it( 'should reset editedTodo on editDone', () => {
+    const wrapper = shallowMount( TodoList, {
+      store,
+      localVue,
+      attachToDocument: true
+    } );
+    const childInput = wrapper.find( '#editTitle' );
+    childInput.trigger( 'click' );
+    childInput.trigger( 'keyup.enter' );
+    expect( wrapper.vm.editedTodo ).toBe( null );
+    wrapper.destroy();
+  } );
+
+  it( 'should rise editCancel on 27(esc) key in edit mode ', () => {
+    const wrapper = shallowMount( TodoList, {
+      store,
+      localVue,
+      attachToDocument: true
+    } );
+    let editCancel = jest.fn();
+    let editTodo = jest.fn();
+    wrapper.setMethods( { editTodo: editTodo, editCancel: editCancel } );
+    wrapper.find( '.todos-list__block_view' ).trigger( 'dblclick' );
+    expect( editTodo ).toHaveBeenCalledWith( fakeData );
+
+    const childInput = wrapper.find( '#editTitle' );
+    childInput.trigger( 'click' );
+    childInput.trigger( 'keyup.esc' );
+    expect( editCancel ).toHaveBeenCalledWith( fakeData );
+    wrapper.destroy();
+  } );
+  it( 'should reset data on esc click ', () => {
+    const wrapper = shallowMount( TodoList, {
+      store, localVue, attachToDocument: true
+    } );
+    wrapper.setData( { editedTodo: fakeData, tmpTitle: fakeData.title, tmpDateExp: fakeData.dateExp } );
+    wrapper.find( '.todos-list__block_view' ).trigger( 'click' );
+
+    const childInput = wrapper.find( '#editTitle' );
+    childInput.trigger( 'click' );
+    childInput.trigger( 'keyup.esc' );
+    expect( wrapper.vm.tmpTitle ).toEqual( fakeData.title );
+    expect( wrapper.vm.tmpDateExp ).toEqual( fakeData.dateExp );
+    expect( wrapper.vm.editedTodo ).toBe( null );
     wrapper.destroy();
   } );
 } );
